98b0f62fc9c9773abf68f90623a7580c7019b875
Wojciech.Czerniak <wojciech.cz7@gmail.com>
2021-04-10 23:43:38 +0200

    Feature Module added - Modules refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 import {FormsModule} from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
-
+import { ProductModule } from './products/product.module';
 @NgModule({
   // w delkaracjach definiujemy, jakie KOMPONENTY NALEŻĄ DO TEGO MODUŁU
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   // Tu deklarujemy jakie zewnętrzne moduły mają być dostępne dla wszystkich komponentów które należą do tego Angular Module
@@ -28,16 +18,11 @@ import { ProductDetailGuard } from './products/product-detail.guard';
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent},
-      { 
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
       { path: 'welcome', component: WelcomeComponent},
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },  // default route
       { path: '**', redirectTo: 'welcome', pathMatch: 'full'} // dla nieistniejącej strony
-    ])
+    ]),
+    ProductModule
   ],
   // defines startup component of the application
   bootstrap: [AppComponent]
